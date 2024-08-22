886f7bd2692180dcc0cf3e51e4fbae8657f87af8
DigiFab AI <digifabai@gmail.com>
2024-08-22 19:21:48 +0000

    added print

--- src/03_03_challenge.py
+++ src/03_03_challenge.py
@@ -22,4 +22,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 # TODO: Visualize one of the decision trees in the forest# Hint: Use plt.figure(figsize=(20, 10)) and tree.plot_tree()
 # # TODO: Use SHAP to explain a single prediction# Hint: Use shap.TreeExplainer and shap_values
 # # TODO: Choose an instance to explain using SHAP
-# # TODO: Save the visualizations and reports in the 'output' folderprint("Challenge: Your task is to complete the code by training the model, visualizing the decision tree, using SHAP for model interpretability, and creating a transparency report.")
+# # TODO: Save the visualizations and reports in the 'output' folder
+
+print("Challenge: Your task is to complete the code by training the model, visualizing the decision tree, using SHAP for model interpretability, and creating a transparency report.")
